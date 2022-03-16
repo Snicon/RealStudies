d53db4da03ad2c7bd27db996a10440dbe6001168
Sixten Peterson <sixten.peterson@winterzone.se>
2022-03-16 21:17:32 +0100

    Getting info from db now

--- base/views.py
+++ base/views.py
@@ -1,23 +1,16 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Matematik 1c'},
-    {'id': 2, 'name': 'Matematik 2c'},
-    {'id': 3, 'name': 'Engelska 7'},
-]
-
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
