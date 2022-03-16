c63b7e179c656dc64d67626b7ab85010dbeb35a0
Sixten Peterson <sixten.peterson@winterzone.se>
2022-03-16 22:18:08 +0100

    Added the ability to remove/delete rooms, very cool indeed

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('room/<int:pk>', views.room, name="room"),
 
     path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<int:pk>', views.updateRoom, name="update-room"),
+    path('delete-room/<int:pk>', views.deleteRoom, name="delete-room"),
 ]
