6fa5bce68d3463329f35b282b8fda63b1732e94a
Sixten Peterson <sixten.peterson@winterzone.se>
2022-03-16 20:22:52 +0100

    New structure for views and routes/urls

--- realstudies/urls.py
+++ realstudies/urls.py
@@ -1,18 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse('Home page')
-
-
-def room(request):
-    return HttpResponse('ROOM')
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('room/', room)
+    path('', include('base.urls'))
 ]
