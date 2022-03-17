24d7ec528785c53f438f247f053c94c5deba6198
Sixten Peterson <sixten.peterson@winterzone.se>
2022-03-17 16:25:35 +0100

    Added acitivty feed, messages and better templating with components such wow

--- base/urls.py
+++ base/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<int:pk>', views.updateRoom, name="update-room"),
     path('delete-room/<int:pk>', views.deleteRoom, name="delete-room"),
+    path('delete-message/<int:pk>', views.deleteMessage, name="delete-message"),
 ]
--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # Create your views here.
@@ -71,14 +71,34 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    # Todo: Filter messages for activity feed.
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    context = {
+        'rooms': rooms,
+        'topics': topics,
+        'room_count': room_count,
+        'room_messages': room_messages,
+    }
+
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
@@ -126,3 +146,18 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    # TODO: Redirect to home page with a flash message instead. Super user support?
+    if request.user != message.user:
+        return HttpResponse('Ser ut som att du inte får göra "på detta viset". :/')
+
+    if request.method == 'POST':
+        message.delete()
+        # TODO: User should be redirected to the room not the home page
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
